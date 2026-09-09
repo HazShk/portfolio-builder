9b8b30ca6b9de4db6b08b8d39aa6bb77d81a32ee
Hassan Shaikh <shaikh.hassan01@outlook.com>
2026-09-09 09:27:43 -0400

    page templated created

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+const fs = require("fs"); //import filesystem module
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const profileDataArgs = process.argv.slice(2); removes the two referenced paths for node and filelaunch
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-  console.log("================");
-
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
